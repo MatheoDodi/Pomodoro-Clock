592c02137b1158ee129404f821ce1ac82fa629c9
Matthew Dodi <matthew.dodi@gmail.com>
2018-07-09 13:41:13 -0700

    added RESET and START button

--- script.js
+++ script.js
@@ -8,14 +8,22 @@ function reset() {
 
 }
 
+function checkSecond() {
+	
+}
+
 function timerFunc() {
+
+	if (minutes < 0) {
+		return timer.innerHTML = `DONE`;
+	}
 
 	if (minutes === 0 && seconds === 0) {
 		return timer.innerHTML = `DONE`;
 	} else if (seconds === 0) {
 		seconds = 59;
 		minutes--;
-	}
+	} 
 
 	if (seconds.toString().length < 2) {
 	timer.innerHTML = `${minutes}:0${seconds}`;
@@ -37,6 +45,14 @@ document.querySelector('#up').addEventListener('click', function() {
 document.querySelector('#down').addEventListener('click', function() {
 	reset();
 	timer.innerHTML = `${minutes}:0${seconds}`;
+	clearInterval(START_GAME);
 	})
+document.querySelector('#downfive').addEventListener('click', function() {
+	minutes = minutes - 5;
+	timer.innerHTML = `${minutes}:${seconds}`;
+	
+	})
+document.querySelector('#start').addEventListener('click', function() {
+	START_GAME = setInterval( timerFunc , 1000);
+})
 
-setInterval( timerFunc , 1000);
